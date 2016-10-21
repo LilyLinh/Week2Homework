3b92a20c1a2db25bfb84918d81c6dfc33250e946
HA CAO THUY LINH <linhhacaothuy@gmail.com>
2016-10-22 00:55:11 +0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
  def index
+ 	Rails.logger.info request.env["HTTP_COOKIE"]
 	@users = User.all
  end
  def new
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
  def create
  	 @user = User.new user_params
  	 if @user.save
+ 	 	session[:user_id] =@user.id
  	 	redirect_to root_path, notice: "Account successfully created"
      else
      	render 'new'
